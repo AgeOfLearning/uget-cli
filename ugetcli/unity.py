9b2c6ef57b4ac1ef743b0919cc52aa7c67588377
leonid.umanskiy <leonid.umanskiy@aofl.com>
2018-12-13 11:30:11 -0800

    Printing unity log files content for easier automation

--- ugetcli/unity.py
+++ ugetcli/unity.py
@@ -46,7 +46,13 @@ class UnityRunner:
         if self.unity_serial:
             arguments.extend(["-serial", self.unity_serial])
 
-        return self._run_editor(arguments, stdout_log_path, stderr_log_path)
+        unity_exitcode = self._run_editor(arguments, stdout_log_path, stderr_log_path)
+
+        if self.debug and unity_exitcode != 0:
+            click.secho("Unity failed to run, printing log files contents in --debug mode")
+            self._debug_print_unity_logs(editor_log_path, stderr_log_path, stderr_log_path)
+
+        return unity_exitcode
 
     def _run_editor(self, options, stdout_log_path, stderr_log_path):
         command_list = [self.unity_path] + options
@@ -57,3 +63,18 @@ class UnityRunner:
         with open(stdout_log_path, "wb") as stdout, open(stderr_log_path, "wb") as stderr:
             process = Popen(command_list, stderr=stderr, stdout=stdout)
             return process.wait()
+
+    @staticmethod
+    def _debug_print_unity_logs(editor_log_path, stdout_log_path, stderr_log_path):
+        UnityRunner._debug_print_unity_log(editor_log_path)
+        UnityRunner._debug_print_unity_log(stdout_log_path)
+        UnityRunner._debug_print_unity_log(stderr_log_path)
+
+    @staticmethod
+    def _debug_print_unity_log(log_path):
+        click.secho("Begin log file contents " + log_path + ": ------------------------------")
+        with open(log_path, "r") as logfile:
+            for line in logfile:
+                print(line)
+        click.secho("End log file contents " + log_path + ": --------------------------------")
+
