4d78813d1047d1449bf965211ccea12dc6aab168
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-12-27 13:58:19 -0800

    Verbosity must be before Properties

--- tests/integration/test_nuget.py
+++ tests/integration/test_nuget.py
@@ -43,8 +43,8 @@ class TestUGetCliNuGetRunner(unittest.TestCase):
         mock_popen.return_value = mock_process
 
         expected_command_str = "nuget.exe pack TestProject.csproj -OutputDirectory Output " \
+                               "-Verbosity normal" \
                                "-Properties Configuration=Debug;unityPackagePath=TestProject.1.0.0.unitypackage " \
-                               "-Verbosity normal"
 
         nuget_runner = NuGetRunner("nuget.exe")
         assert nuget_runner.pack("TestProject.csproj", "Output", "Debug", "TestProject.1.0.0.unitypackage") == 0
