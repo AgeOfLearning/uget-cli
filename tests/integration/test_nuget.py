ba566ccb1539a962493ab72c0c7a330d5917f68a
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-12-27 14:03:57 -0800

    Removing space after the command.

--- tests/integration/test_nuget.py
+++ tests/integration/test_nuget.py
@@ -44,7 +44,7 @@ class TestUGetCliNuGetRunner(unittest.TestCase):
 
         expected_command_str = "nuget.exe pack TestProject.csproj -OutputDirectory Output " \
                                "-Verbosity normal " \
-                               "-Properties Configuration=Debug;unityPackagePath=TestProject.1.0.0.unitypackage " \
+                               "-Properties Configuration=Debug;unityPackagePath=TestProject.1.0.0.unitypackage" \
 
         nuget_runner = NuGetRunner("nuget.exe")
         assert nuget_runner.pack("TestProject.csproj", "Output", "Debug", "TestProject.1.0.0.unitypackage") == 0
